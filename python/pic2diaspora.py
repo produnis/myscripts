8711d413587155ca929eb3af317eaf2907007fe3
Joe Slam <schlarmann@produnis.de>
2017-12-10 14:08:45 +0100

    added txt-support for messaging along with pictures

--- python/pic2diaspora.py
+++ python/pic2diaspora.py
@@ -6,6 +6,7 @@ import sys
 #
 # This script posts pictures from a given directory
 # to a given DIASPORA-Account. It posts one picture per round.
+# Subfolders are not supported.
 #
 # Requires:
 #		- python3
@@ -14,19 +15,23 @@ import sys
 # Usage:
 #		# script will fail if it is not called from picturefolder!!
 #		$:>  cd /path/to/picturefolder							
-# 		$:>  /path/to/pic2diaspora.py /path/to/picturefolder
+# 		$:>  /path/to/pic2diaspora.py
 #
 # This script will create the file "0archive_posted.txt"
-# in the given picture directory (check for write permission). In this textfile,
-# filenames of pictures already posted are stored.
-# This script won't upload any pictures included in "0archive_posted.txt"
+# in the given picture directory (check for write permission). 
+# In this textfile, filenames of pictures already posted are stored.
+#
 # The script looks up all pictures in the given directory
 # and compares their filenames with "0archive_posted.txt"
-# The script will post the first "new" pictures it founds to Diaspora and exits. So, 
-# one picture is posted per round. Thus, the script is ment to be run via cronjob.
+# This script won't upload any pictures included in "0archive_posted.txt"
+# The script will take the first "new" pictures it finds, post it to Diaspora and exit. 
+# If there is a .txt-file with the same basename as the picture-file (e.g. "picture1.jpg" and "picture1.txt")
+# The txt-file's content will be posted along with the picture.
+# 
+# So, one picture is posted per round. Thus, the script is ment to be run via cronjob.
 #  # Cron.Example:
-#  # post every 6 hours a picture
-#  0 */6 * * * 	cd /path/to/picturefolder; /path/to/pic2diaspora.py /path/to/picturefolder
+#  # post a picture every 6 hours 
+#  0 */6 * * * 	cd /path/to/picturefolder; /path/to/pic2diaspora.py 
 #
 #---------------------------------------------
 # 		CHANGE TO FIT YOUR SETTINGS
@@ -45,14 +50,7 @@ standardmessage = '#cool #hashtags'		# message posted with each photo, e.g. "#ns
 # # #    M A I N    L O O P    # # # #
 #---------------------------------------------
 #
-# check if the picture directory is set
-picdir = sys.argv[1]
-if len(picdir) < 1:
-    print("Please specify picture directory")
-    sys.exit()
-elif picdir == ".":
-	print("Please type full path to picture directory")
-	sys.exit()
+picdir = os.getcwd()
 print('picture directory is %s' % (picdir))
 
 
@@ -82,7 +80,18 @@ for pics in pictures:
 			print('Ready to post new picture: %s' % (pics))
 			pic_path = '%s/%s' % (picdir, pics)
 			
-			
+			# check if there is a .txt for a message to post with this pic
+			pic_base = os.path.splitext(pic_path)[0] # remove picture suffix from filename
+			pic_txtfile  = "%s.txt" % (pic_base)     # add .txt
+			print("Looking for txt-file %s \n" % (pic_txtfile))
+			if os.path.exists(pic_txtfile) == True:
+				print("Found txt file!\n")
+				pic_txt = open(pic_txtfile).read()
+				textmessage= '%s \n %s' % (pic_txt, standardmessage)
+			else:
+				print("No txt-file found. Posting Image only\n")
+				textmessage=standardmessage
+	
 			# post pic to diaspora
 			print('Upload pic %s with path %s to Diaspora' % (pics, pic_path))
 			connection = diaspy.connection.Connection(pod=podurl, username=poduser, password=poduserpwd)
