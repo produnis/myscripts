72798aa21fb7c07f45e02f257112bcd0043f33d9
Joe Slam <schlarmann@produnis.de>
2019-01-27 15:42:56 +0100

    mediawiki 1.32 with login to API and as normal user

--- python/mwc2pdf.py
+++ python/mwc2pdf.py
@@ -1,54 +1,60 @@
-#!/usr/bin/env python2
-#
-## Usage:
-## 1) Change the three lines 49-51
-##    to fit your situation
-## 2) open terminal
-## 3) ./PATH/TO/mwc2pdf.py
-#
-#
+#!/usr/bin/env python3
 #------------------------------------------------
 ## 					Welcome to mwc2pdf
 ## ----------------------------------------------
-## Media Wiki Category To PDF (MWC2PDF)
-## is a php script that exports a Category from MediaWiki
-## into PDF, including all Pages of that category as well as 
-## all pages from subcategories.
-## mwc2pdf uses MediaWiki's  "api.php" to collect 
-## all data and create a "pagetree".
-## mwc2pdf prints out every item of that pagetree 
-## into a single pdf-file using pdfkit ("wkhtmltopdf").
-## It than combines all single pdf-files into 
-## one single pdf-file called "MWC2PDF-Output.pdf" using "PyPDF2".
+## Media Wiki Category To PDF (mwc2pdf)...
+## - is a php script that exports a Category from MediaWiki
+##   into PDF, including all pages of that category as well as 
+##   all pages from subcategories.
+## - mwc2pdf uses MediaWiki's  "api.php" to collect all data 
+##   and it creates a sorted "pagetree" without doubles/twin entries.
+## - mwc2pdf downloads every item of that pagetree as a PDF-file
+##   using mediawiki's "Mpdf" extension.
+## - It then combines all pdf-files into one single pdf-file
+##   called "MWC2PDF-Output.pdf" 
+## - It uses "PyPDF2" to set some bookmarks while creating
+##   the single pdf-file.
 ##
+##   All files are generated in the directory you call mwc2pdf.py from
+##   So, make sure you have write-permission to that path!
 ## -----------------------------------------------
-## Requires:
-## - mediawiki >= 1.27
-## - wkhtmltopdf
-## - Python 2  with:
-## - -- pdfkit
+##
+## REQUIERES:
+## ##########
+## - mediawiki >= 1.32, with:
+## -- Mpdf extension
+## -- you having set up Special:BotPasswords for your account
+## - Python3  with:
 ## - -- PyPDF2
-## - -- urllib2
+##
+## if your Wiki has long pages, 
+## you might want to set
+##      max_execution_time = 360
+## in php.ini to avoid timeout-errors
+## while rendering the PDF-file.
 ## -------------------------
-## Written by Joe Slam 2015-2016
+## Written by Joe Slam 2015-2019
 ## Licence: GPL v3
 ## -------------------------
 ##
-## Usage:
-## 1) Change the three lines in 49-51 
+##
+## USAGE:
+## ######
+## 1) Change the lines 53-60
 ##    to fit your situation
 ## 2) open terminal
-## 3) $ /PATH/TO/mwc2pdf.py 
-##           
-## All files are generated in the directory you call mwc2pdf.py from
-## So, make sure you have write-permission to that path!
+## 3) ./PATH/TO/mwc2pdf.py
+##
 ####################################################################
 
 # + + + + + + + + + + + + + + + + + + + + + + +
 ## Change this vars to fit your system
-mywiki 			= "http://192.168.0.4/produniswiki/"# URL to your wiki's index.php
-kategorie 		= "Kategorie:Hauptkategorie"				# Which Category to grab?
-kategorie_word 	= "Kategorie:"						# What is "Category:" in your wiki's language?
+mywiki 			= "https://produnis.spdns.org/produniswiki/"	# URL to your wiki's index.php
+kategorie 		= "Kategorie:Hauptkategorie"					# Which Category to grab?
+kategorie_word 	= "Kategorie:"									# What is "Category:" in your wiki's language?
+username 		= 'produnis'									# Username to login with
+userpwd			= '6hansch#'									# Userpassword
+botpwd 			= 'pdfexport@jv6sto7cjvp741f9fvgtloaom5klttph'	# Password of Special:BotPasswords in the style BOTNAME@password
 # + + + + + + + + + + + + + + + + + + + + + + +
 keep_tmp_pdfs = "no"  	# switch "no" / "yes" to keep tmp-pdf-files of every page
 
@@ -58,35 +64,80 @@ keep_tmp_pdfs = "no"  	# switch "no" / "yes" to keep tmp-pdf-files of every page
 # ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! ! !  ! ! ! !
 ##----------------------------------------------
 import os
-import pdfkit
 from PyPDF2 import utils, PdfFileReader, PdfFileWriter
 import sys
-import time
-import urllib2  				
-import xml.etree.ElementTree as ET
+import datetime
+import time			
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+right_now 	= datetime.datetime.now()
+index_url 	= mywiki + "index.php"
+api_url 	= mywiki + 'api.php'
+
+#-----------------------------------------------------------------
+## Login to MediaWiki API
+# we use the API to get some Info about the Categories and Pages. 
+session = requests.Session()
+
+# get login token
+#print("session cookies before r1: ", dict(session.cookies))
+r1 = session.get(api_url, params={
+    'format': 'json',
+    'action': 'query',
+    'meta': 'tokens',
+    'type': 'login',
+})
+#r1.raise_for_status()
+#print(r1.json())
+#print("session cookies after r1: ", dict(session.cookies))
+# log in
+r2 = session.post(api_url, data={
+    'format': 'json',
+    'action': 'login',
+    'lgname': username,
+    'lgpassword': botpwd,
+    'lgtoken': r1.json()['query']['tokens']['logintoken'],
+})
+
+#print("session cookies after r2: ", dict(session.cookies))
+#print("request body: ", r2.request.body)
+if r2.json()['login']['result'] != 'Success':
+    raise RuntimeError(r2.json())
+#-----------------------------------------------------------------
 
 
 
-## API -Commands
-#------------------
-# List all Subcategories
-cmd_subcat = "api.php?action=query&format=xml&list=categorymembers&cmlimit=500&cmtitle="
-# List all Pages of Category
-cmd_catinfo = "api.php?action=query&format=xml&prop=pageprops&titles="
-# Get URL from pageid
-cmd_geturlfromID = "api.php?action=query&format=xml&inprop=url&prop=info&pageids="
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# login as "normal" user (to get PDFs)
+# we need to log in to mediawiki as "normal" user in order to
+# call the Mpdf-Extension
+payload = {
+	'wpName': username,
+	'wpPassword': userpwd,
+	'wploginattempt': 'Log in',
+	'wpEditToken': "+\\",
+	'title': "Special:UserLogin",
+	'authAction': "login",
+	'force': "",
+	'wpForceHttps': "1",
+	'wpFromhttp': "1",
+    #'wpLoginToken': '',
+	}
 
-
+def get_login_token(raw_resp):
+	soup = bs(raw_resp.text, 'lxml')
+	token = [n.get('value', '') for n in soup.find_all('input')
+		if n.get('name', '') == 'wpLoginToken']
+	return token[0]
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
+	
 
 
 
 # Functions
 #-------------------
-def getCategoryID(myxml):
-	return(myxml[0][0][0].get('pageid'))
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
 def startsWith(haystack, needle):
 	 # this function checks if the string 
 	 # "$hayshack" starts with the letters "$neddle"
@@ -95,41 +146,32 @@ def startsWith(haystack, needle):
      return (haystack[:length] == needle)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-def wikiHTMLprint(the_title):
-	# This function takes a pageid and 
-	# returns the "printable"-URL of that
-	# page/category
-	command = "%s%s%s" % (mywiki, cmd_geturlfromID, the_title)
-	mycontent = urllib2.urlopen(command)
-	mydata = mycontent.read()
-	mycontent.close()
-	myxml  = ET.fromstring(mydata)
-	output = myxml[0][0][0].get('editurl')
-	output = output.replace("&action=edit", "&printable=yes")
-	#the_title = urllib2.quote(the_title)
-	#output = "%s/index.php?title=%s&printable=yes" % (mywiki, the_title)
-	return(output);
-	
 #---- end of functions --------------
 	
-
-
 
 
 		
 #### M A I N   S C R I P T
 #-------------------------
-print "\n\nCollecting data for %s \n\n" % (kategorie)
+print("\n\nCollecting data for %s \n\n" % (kategorie))
 
 # Get pageid of start-category
-the_url = "%s%s%s" % (mywiki, cmd_catinfo, kategorie)  	# built up URL
-the_url = the_url.encode('utf-8') 					   	# encode URL to UTF-8
-mycontent = urllib2.urlopen(the_url)					# open URL
-mydata = mycontent.read()								# read Content
-mycontent.close()										# close URL
+MyParams = {
+	"action": "query",
+	"format": "json",
+	"prop": "pageprops",
+	"titles": kategorie
+}
+mycontent = session.get(api_url, params=MyParams)
+mydata=mycontent.json
+for key in mydata()['query']['pages']:
+	#print("loop ", key)
+	current_parent = key
+print("current parent: %s" % (current_parent))
+#-------------------------
 
-data = ET.fromstring(mydata)
-current_parent = getCategoryID(data)					# get "pagied" of startpoint
+
+
 
 
 # Setting up all important vars() and arrays()
@@ -172,19 +214,23 @@ x = x + 1
 
 while done == False:
 	
-	#get pageid of current category
+	#get content of current category
 	current_parent = loop_id[y]
-	the_url = "%s%s%s" % (mywiki, cmd_subcat, loop_list[y])
-	the_url = the_url.encode('utf-8')
-	mycontent = urllib2.urlopen(the_url)
-	mydata = mycontent.read()
-	mycontent.close()
-	data = ET.fromstring(mydata)
-
-	for child in data.iter('cm'):
-		neuer_titel = child.get('title')
-		neue_pageid = child.get('pageid')
-
+	MyParams = {
+		"action": "query",
+		"format": "json",
+		"list": "categorymembers",
+		"cmlimit": 500,
+		"cmpageid": current_parent
+	}
+	mycontent = session.get(api_url, params=MyParams)
+	mydata=mycontent.json
+	#print("current parent: ", mydata()['query']['categorymembers'])
+	# get id and title for each content
+	for item in mydata()['query']['categorymembers']:
+		#print(item['pageid'], " - ", item['title'])
+		neuer_titel = item['title']
+		neue_pageid = item['pageid']
 	
 		# is this a Page?
 		# ---------------------------------------------------
@@ -224,12 +270,11 @@ while done == False:
 				the_parentid.append(current_parent)
 				level_dummy = current_level +1
 				the_level.append(level_dummy)
-				is_category.append("C") 
-		
+				is_category.append("C") 	
 		x = x + 1
 	y = y + 1 
- 	current_level = current_level + 1;
- 	if y == (len(loop_list)):
+	current_level = current_level + 1
+	if y == (len(loop_list)):
 		print("Ende")
 		done =True
 #----------------------------
@@ -253,6 +298,8 @@ while j < i:
 #---------------------------------
 
 
+
+
 #######################################
 # Printing into PDF file
 #______________________________________
@@ -260,31 +307,31 @@ gesamtzahl = len(the_prefix)
 seitenzaehler  = 0
 artikelzaehler = 0
 output_pdf_stream = PdfFileWriter()
-options = {
-    'page-size': 'A4',
-    'margin-top': '0.5in',
-    'margin-right': '0.5in',
-    'margin-bottom': '0.5in',
-    'margin-left': '0.5in',
-    'encoding': "UTF-8",
-    'no-outline': None
-}
-
 
 for f in the_prefix:
 	prozente = round(artikelzaehler*100/gesamtzahl,3)
 	print("Processing %s (%s of %s) / %s %%" % (the_names[artikelzaehler], (artikelzaehler+1), gesamtzahl, prozente) )
 
-	# Getting printable URL
-	pdf_url = wikiHTMLprint(the_pageids[artikelzaehler])
-	
 	# Getting PDF-Version of URL
 	the_prefix_dummy = "%05d" % (artikelzaehler)
 	the_filename = "%s-%s-%s_%s.mwcpdf.pdf" % (the_prefix_dummy, the_prefix[artikelzaehler], is_category[artikelzaehler], the_pageids[artikelzaehler])
-	current_pdf = pdfkit.from_url(pdf_url, the_filename, options=options)
+	the_html_name= "%s-%s-%s_%s.mwcpdf.html" % (the_prefix_dummy, the_prefix[artikelzaehler], is_category[artikelzaehler], the_pageids[artikelzaehler])
+
+	# log in to mediawiki as "normal user"
+	with requests.session() as s:
+		resp = s.get(index_url + '?title=Spezial:UserLogin')
+		payload['wpLoginToken'] = get_login_token(resp)
+		response_post = s.post(index_url + '?title=Spezial:UserLogin&action=submitlogin&type=login', data=payload)
+		
+		# get pdf-version of page, using Mpdf-Extension
+		response = s.get(index_url + '?title=' + the_names[artikelzaehler] + '&action=mpdf')
+		with open(the_filename, 'wb') as f:
+			f.write(response.content)
 	
+	
+		
 	# Reading into PdfFileReader
-	current_pdf = PdfFileReader(open(the_filename, "rb"))
+	current_pdf = PdfFileReader(open(the_filename, "rb"),strict=False)
 	
 	# Setting Bookmarks
 	the_bookmarkname = the_names[artikelzaehler].encode('utf-8')
@@ -307,18 +354,18 @@ for f in the_prefix:
 #------ Ende merging PDF with Bookmarks
 	
 try:
-	output_pdf_name = "MWC2PDF-Output.pdf"
+	output_pdf_name = "MWC2PDF-Output-%s-%s-%s.pdf" % (right_now.year, right_now.month, right_now.day)
 	output_pdf_file = open(output_pdf_name, "wb")
 	output_pdf_stream.write(output_pdf_file)
 finally:
 	output_pdf_file.close()
 
-print "%s successfully created." % output_pdf_name
+print("%s successfully created." % output_pdf_name)
 
 # End printing into PDF-File
 ###############################################################
 
 
-
+# https://stackoverflow.com/a/38378803/1493264  how to login to Mediawiki with Python
 # http://superuser.com/questions/1009939/how-to-merge-pdfs-and-create-bookmarks-for-each-input-file-in-output-file-linu
 # https://pypi.python.org/pypi/pdfkit/0.4.1
