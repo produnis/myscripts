c2c4eed8642590f9c12307897ab8f7a5ec58b92a
Joe Slam <schlarmann@produnis.de>
2019-02-23 12:00:59 +0100

    polishing

--- python/pic2mastodon.py
+++ python/pic2mastodon.py
@@ -5,7 +5,7 @@ import sys
 ########################################################
 #
 # This script posts pictures from a given directory
-# to a given Mastodon-Account. It posts one picture per round.
+# to a given Mastodon-account. It posts one picture per round.
 #
 # Requires:
 #		- python3
@@ -19,11 +19,13 @@ import sys
 # This script will create the file "0archive_mastodon.txt"
 # in the given picture directory (check for write permission). In this textfile,
 # filenames of pictures already posted are stored.
-# This script won't upload any pictures included in "0archive_mastodon.txt"
 # The script looks up all pictures in the given directory
-# and compares their filenames with "0archive_mastodon.txt"
-# The script will post the first "new" pictures it founds to Mastodon and exits. So, 
-# one picture is posted per round. Thus, the script is ment to be run via cronjob.
+# and compares their filenames with "0archive_mastodon.txt".
+# The script won't upload any pictures of filenames included in "0archive_mastodon.txt"
+# The script will post the first "new" pictures it finds to mastodon, writes the filename into "0archive_mastodon.txt" and exits. 
+# If there is a .txt-file with the same basename as the picture-file (e.g. "picture1.jpg" and "picture1.txt")
+# The txt-file's content will be posted as a Text along with the picture.
+# So, one picture is posted per round. Thus, the script is ment to be run via cronjob.
 #  # Cron.Example:
 #  # post every 6 hours a picture
 #  0 */6 * * * 	cd /path/to/picturefolder; /path/to/pic2mastodon.py
@@ -31,12 +33,12 @@ import sys
 #---------------------------------------------
 # 		CHANGE TO FIT YOUR SETTINGS
 #---------------------------------------------
-podurl 		= 'https://mastodon.url' 	# The URL of your account's pod
-poduser 	= 'USERNAME'				# Username	
-poduserpwd 	= 'SUPERSECRET'				# Password
-standardmessage = '#cool #hastags '		# message posted with each photo, e.g. "#nsfw"
-visibility 		= "public"				# "direct", "private", "unlisted", "public"
-access_token 	= "YY XX ZZ " 			# get the api-token via your account website
+podurl 			= 'https://mastodon.url'	# The URL of your account's pod
+standardmessage = '#cool #hastags '			# message posted with each photo, e.g. "#nsfw"
+visibility 		= "public"					# "direct", "private", "unlisted", "public"
+access_token 	= "YY XX ZZ " 				# The acces-token is needed to login/post to your account
+											# You can e.g. use https://takahashim.github.io/mastodon-access-token/
+											# to generate your access-key.
 #---------------------------------------------
 #########################################################
 # no need to change anything after here
@@ -73,7 +75,7 @@ archiveposted = open(archive_path).read()
 pictures = os.listdir(picdir)
 for pics in pictures:
 	# check if this pic is really a pic
-	if pics.lower().endswith(('.png', '.jpg', '.gif', '.jpe', '.jpeg')):
+	if pics.lower().endswith(('.png', '.jpg', '.gif', '.mp4', '.jpe', '.jpeg')):
 		# check if pic was already posted
 		if pics in archiveposted:
 			print('picture already posted: %s' % (pics))
